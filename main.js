b8d6c40ac32374ef3bdef7964a9bcb1759fb1b50
JoyHaddad <joy4haddad@gmail.com>
2024-05-06 12:37:27 -0700

    test the images

--- main.js
+++ main.js
@@ -25,33 +25,39 @@ function fetchData() {
       if (data.results) {
         const postsContainer = document.querySelector(".posts");
         postsContainer.innerHTML = ""; // Clear previous posts
-        counter = 1;
-        data.results.slice(0, 5).forEach((article) => {
+        counter = 0;
+        while (counter < data.results.length && counter < 5) {
+          let article = data.results[counter];
           let articleImg = "";
           if (
             article.media &&
             article.media.length > 0 &&
             article.media[0]["media-metadata"] &&
-            article.media[0]["media-metadata"].length > 0
+            article.media[0]["media-metadata"].length > 0 &&
+            article.media[0]["media-metadata"][0].url &&
+            article.media[0]["type"] === "image"
           ) {
             articleImg = article.media[0]["media-metadata"][0].url;
-          }
-          const postHTML = `
+            const postHTML = `
                     <div class="post">
                         <div class="articleHeader">
-                            <div class="articleTitle">${counter}) ${article.title}</div>
-                            <div class="articleDate">${article.published_date}</div>
+                            <div class="articleTitle">${counter + 1}) ${article.title}</div>
+                            <div class="articleDate">${
+                              article.published_date
+                            }</div>
                         </div>
                         <div style="display:flex; align-items: center; margin-left: 10px; margin-right: 10px;">
                             <div class="articleImg"><img src="${articleImg}" alt="Article Image"></div>
-                            <div class="articleAbstract">${article.abstract}</div>
+                            <div class="articleAbstract">${
+                              article.abstract
+                            }</div>
                         </div>
                     </div>
                 `;
-
-          postsContainer.insertAdjacentHTML("beforeend", postHTML);
+            postsContainer.insertAdjacentHTML("beforeend", postHTML);
+          }
           counter++;
-        });
+        }
       }
     })
     .catch((error) => console.log("Error:", error));
